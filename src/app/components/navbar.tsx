72b93d705c7479d11190b6eb2d3b2674cc8011d7
Justin Singh <justin.singh@acertusdelivers.com>
2024-05-26 16:49:03 -0700

    added media queries to navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,10 +4,10 @@ import { Typography } from '../ui/typography';
 export const Navbar = () => {
   return (
     <nav className="bg-primary-400 p-4 pb-5 gap-4 flex justify-center relative">
-      <Link href="/" className="absolute no-underline">
+      <Link href="/" className="absolute no-underline max-md:static">
         <Typography variant="h3">VMK Builders & CO</Typography>
       </Link>
-      <div className="flex gap-4 justify-end w-full">
+      <div className="flex gap-4 justify-end w-full max-md:hidden">
         <Link href="/">Home</Link>
         <Link href="/projects">Projects</Link>
       </div>
