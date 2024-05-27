c97cf80f3e30ecaad6b59147ef0a7d9eee084c69
Justin Singh <justin.singh@acertusdelivers.com>
2024-05-26 17:34:10 -0700

    showing nav links again

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -3,11 +3,13 @@ import { Typography } from '../ui/typography';
 
 export const Navbar = () => {
   return (
-    <nav className="bg-primary-400 p-4 pb-5 gap-4 flex justify-center relative">
+    <nav className="bg-primary-400 p-4 pb-5 gap-4 flex justify-center relative max-md:flex-col">
       <Link href="/" className="absolute no-underline max-md:static">
-        <Typography variant="h3">VMK Builders & CO</Typography>
+        <Typography variant="h3" className="text-center">
+          VMK Builders & CO
+        </Typography>
       </Link>
-      <div className="flex gap-4 justify-end w-full max-md:hidden">
+      <div className="flex gap-4 justify-end w-full max-md:justify-center">
         <Link href="/">Home</Link>
         <Link href="/projects">Projects</Link>
       </div>
