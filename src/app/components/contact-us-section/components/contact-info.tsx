9f963c154db445a4826eaf5d188163461e36a282
Justin Singh <justin.singh@acertusdelivers.com>
2024-05-26 17:57:53 -0700

    added temp contact info

--- src/app/components/contact-us-section/components/contact-info.tsx
+++ src/app/components/contact-us-section/components/contact-info.tsx
@@ -7,7 +7,7 @@ export const ContactInfo = () => {
       <Typography variant="h3" className="text-primary-400">
         VMK Builders & CO. License #000000
       </Typography>
-      <Link href="tel:9165348218" className="text-primary-400">
+      <Link href="tel:555555555" className="text-primary-400">
         (555) 555-5555
       </Link>
     </div>
