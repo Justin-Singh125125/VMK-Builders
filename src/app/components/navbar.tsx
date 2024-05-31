7f01ef99d2e9ceeb4bc2a6cc6cac9953ecb6534d
Justin Singh <justin.singh@acertusdelivers.com>
2024-05-31 14:26:44 -0700

    added image

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,13 +1,13 @@
 import { Link } from '../ui/link';
+import Image from 'next/image';
 import { Typography } from '../ui/typography';
+import Logo from './logo.png';
 
 export const Navbar = () => {
   return (
-    <nav className="bg-primary-400 p-4 pb-5 gap-4 flex justify-center relative max-md:flex-col">
-      <Link href="/" className="absolute no-underline max-md:static">
-        <Typography variant="h3" className="text-center">
-          VMK Builders & CO
-        </Typography>
+    <nav className="bg-primary-400 p-4 pb-5 h-[120px] gap-4 flex justify-center relative max-md:flex-col">
+      <Link href="/" className="no-underline max-md:static absolute">
+        <Image src={Logo} width={500} height={500} alt="Picture of the author" />
       </Link>
       <div className="flex gap-4 justify-end w-full max-md:justify-center">
         <Link href="/">Home</Link>
