da26054b53f25b41d89ad116d07a407d73d611a4
Justin Singh <justin.singh@acertusdelivers.com>
2024-05-30 19:17:18 -0700

    test

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,8 @@
+// To stream responses you must use Route Handlers in the App Router, even if the rest of your app uses the Pages Router.
+ 
+export const dynamic = 'force-dynamic'; // static by default, unless reading the request
+export const runtime = 'nodejs';
+
+export function GET(request: Request) {
+  return new Response(`Hello world`);
+}
